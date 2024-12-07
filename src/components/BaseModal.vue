8580e3a7814265cdeb84d9cf1888e9c1dc0b77b1
Yoki Jati Perkasa <yokijati@gmail.com>
2024-12-07 20:02:08 +0700

    Module 4 Completed

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,12 +1,64 @@
 <template>
-  <div
-    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
-  >
-    <div class="p-4 bg-white mt-500 self-start mt-32 max-w-screen">
-      <slot />
-      <button class="text-white bg-orange-500 py-2 mt-10 px-6">Close</button>
-    </div>
-  </div>
+  <Teleport to="body">
+    <Transition name="modal-outer">
+      <div
+        v-show="showModal"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+      >
+        <Transition name="modal-inner">
+          <div
+            v-if="showModal"
+            class="p-4 bg-white mt-500 self-start mt-32 max-w-screen"
+          >
+            <slot />
+            <button
+              class="text-white bg-orange-500 py-2 mt-10 px-6"
+              @click="$emit('close-modal')"
+            >
+              Close
+            </button>
+          </div>
+        </Transition>
+      </div>
+    </Transition>
+  </Teleport>
 </template>
 
-<script setup></script>
+<script setup>
+defineEmits(["close-modal"]);
+defineProps({
+  showModal: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-outer-enter-from,
+.modal-outer-enter-to {
+  opacity: 0;
+}
+
+.modal-inner-enter-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+
+.modal-inner-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-inner-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.modal-inner-leave-to {
+  transform: scale(0.8);
+}
+</style>
--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -13,14 +13,39 @@
       <div class="flex gap-3 flex-1 justify-end">
         <i
           class="fa-solid fa-circle-info text-xl text-black hover:text-weather-secondary duration-500 cursor-pointer"
+          @click="toggleModal"
         ></i>
         <i
           class="fa-solid fa-plus text-xl text-black hover:text-weather-secondary duration-500 cursor-pointer ms-6"
         ></i>
       </div>
 
-      <BaseModal>
-        <h1 class="text-black">Hello From Modal Here</h1>
+      <BaseModal :showModal="showModal" @close-modal="toggleModal">
+        <div class="text-black">
+          <h1 class="text-2xl mb-1">About:</h1>
+          <p class="mb-4">
+            The Local Weather allows you to track the current and future weather
+            of cities of your choosing.
+          </p>
+          <h2 class="text-2xl">How it works:</h2>
+          <ol class="list-decimal list-inside mb-4">
+            <li>Search for a city by entering the name into the search bar.</li>
+            <li>
+              Select a city within the results, this will take you to the
+              current weather for your selection
+            </li>
+            <li>
+              Track the city by clicking on the "+" icon in the top right. This
+              will save the city to view at a later time on the Home Page
+            </li>
+          </ol>
+          <h2 class="text-2xl">Removing a City:</h2>
+          <p>
+            If you no longer wish to track a city, simply select the city within
+            the home page. At the bottom of the page, there will be a button to
+            remove the city.
+          </p>
+        </div>
       </BaseModal>
     </nav>
   </header>
@@ -29,4 +54,11 @@
 <script setup>
 import { RouterLink } from "vue-router";
 import BaseModal from "./BaseModal.vue";
+import { ref } from "vue";
+
+// Variabel Modal State
+const showModal = ref(null);
+const toggleModal = () => {
+  showModal.value = !showModal.value;
+};
 </script>
